ba3fcc0d5c785f606d82ee4a4891bbaa80711d7d
Serhat <serhatpotur1@gmail.com>
2022-10-16 17:33:37 +0300

    Jwt 1

---
+++ src/demoProjects/rentACar/Application/Features/Auths/Rules/AuthsBusinessRules.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Security.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Rules
+{
+    public class AuthsBusinessRules
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AuthsBusinessRules(IUserRepository userRepository)
+        {
+        _userRepository=userRepository;
+        }
+
+        public async Task EmailCanNotBeDublicatedWhenRegistered(string email) // email kullanılıyorsa
+        {
+            User? user = await _userRepository.GetAsync(x => x.Email == email);
+            if (user != null) throw new BusinessException("Mail alread exists");
+        }
+    }
+}
--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Core.Security.Enums;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -14,6 +16,11 @@ namespace Persistence.Contexts
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Model> Models { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
@@ -49,6 +56,34 @@ namespace Persistence.Contexts
                 a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
 
                 a.HasOne(p => p.Brand); // Modelin 1 tane markası var
+            });
+            modelBuilder.Entity<User>(a =>
+            {
+                a.ToTable("Users").HasKey(k => k.Id);
+                a.Property(p => p.FirstName).HasColumnName("FirstName");
+                a.Property(p => p.LastName).HasColumnName("LastName");
+                a.Property(p => p.Email).HasColumnName("Email");
+                a.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                a.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                a.Property(p => p.Status).HasColumnName("Status");
+                a.Property(p => p.AuthenticatorType).HasColumnName("AuthenticatorType");
+                a.HasMany(p => p.UserOperationClaims);
+                a.HasMany(p => p.RefreshTokens);
+
+            });
+            modelBuilder.Entity<OperationClaim>(a =>
+            {
+                a.ToTable("OperationClaims").HasKey(k => k.Id);
+                a.Property(p => p.Name).HasColumnName("Name");
+
+            });
+            modelBuilder.Entity<UserOperationClaim>(a =>
+            {
+                a.ToTable("UserOperationClaims").HasKey(k => k.Id);
+                a.Property(p => p.UserId).HasColumnName("UserId");
+                a.Property(p => p.OperationClaimId).HasColumnName("OperationClaimId");
+                a.HasOne(p => p.User);
+                a.HasOne(p => p.OperationClaim);
             });
 
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
